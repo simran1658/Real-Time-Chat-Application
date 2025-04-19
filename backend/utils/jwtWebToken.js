24a16d9c3c91be03620598c793a0116f8dc8afdb
Simran <singhsim1658ran@gmail.com>
2025-04-19 16:31:35 +0530

    #

--- backend/utils/jwtWebToken.js
+++ backend/utils/jwtWebToken.js
@@ -7,7 +7,7 @@ const jwtToken=(userId,res)=>{
         httpOnly:true, //this will make the cookie http only so that it can't be accessed by the frontend
         maxAge:30*24*60*60*1000, //this will set the time for the cookie to expire in 30 days
         sameSite:'strict', //this will check if the site is same or not //strict means it will only work on the same site
-        secure:process.env.SECURE !== 'development' //this will check if the environment is in development or not
+        secure:true //this will check if the environment is in development or not
     })
 }
 export default jwtToken; //this will export the jwtToken function
